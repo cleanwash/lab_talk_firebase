32e870b5307fc72cc854a62b51179839266db599
Jayone Lee <kenbugless@gmail.com>
2025-02-08 16:33:26 +0900

    feat:파이어베이스 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lab_talk_firebase/core/util/routing/router.dart';
+import 'package:lab_talk_firebase/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
