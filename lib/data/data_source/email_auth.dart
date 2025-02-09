58fe83488b9dc5a482ac9dbcae71706f082c6037
Jayone Lee <kenbugless@gmail.com>
2025-02-09 13:27:22 +0900

    feat:소셜 로그인, 이메일 로그인 추가

---
+++ lib/data/data_source/email_auth.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class EmailAuth {
+  User? currentUser() {
+    return FirebaseAuth.instance.currentUser;
+  }
+
+  Future<void> signUp(
+      {required String email,
+      required String password,
+      required Function() onSuccess,
+      required Function(String err) onError}) async {
+    if (email.isEmpty) {
+      onError("이메일을 입력해 주세요.");
+      return;
+    } else if (password.isEmpty) {
+      onError("비밀번호를 입력해 주세요.");
+      return;
+    }
+
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      onSuccess();
+    } on FirebaseAuthException catch (e) {
+      onError(e.message!);
+    } catch (e) {
+      onError(e.toString());
+    }
+  }
+
+  Future<void> signIn({
+    required String email,
+    required String password,
+    required Function() onSuccess,
+    required Function(String err) onError,
+  }) async {
+    if (email.isEmpty) {
+      onError('이메일을 입력해주세요.');
+      return;
+    } else if (password.isEmpty) {
+      onError('비밀번호를 입력해주세요.');
+      return;
+    }
+  }
+}
